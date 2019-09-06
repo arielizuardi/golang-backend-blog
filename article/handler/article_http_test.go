d49470c39fa700b2343dc21342b8686006f1c66f
Arie Lizuardi <arie.lizuadi@gmail.com>
2019-09-06 07:28:36 +0700

    Arie | Complete handler testing

--- article/handler/article_http_test.go
+++ article/handler/article_http_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -51,5 +52,95 @@ func TestArticleHandler_HandleCreateArticle_UsecaseReturnError(t *testing.T) {
 	if assert.NoError(t, h.HandleCreateArticle(c)) {
 		assert.Equal(t, http.StatusBadRequest, rec.Code)
 		assert.Equal(t, `{"status":400,"message":"","data":null}`, strings.Trim(rec.Body.String(), "\n"))
+	}
+}
+
+func TestArticleHandler_HandleGetArticleByID(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/articles/:article_id")
+	c.SetParamNames("article_id")
+	c.SetParamValues("1")
+
+	result := &model.Article{Title: "my-title", Content: "my-content", Author: "my-author"}
+	u := new(mocks.Usecase)
+	u.On("GetArticleByID", 1).Return(result, nil)
+
+	h := handler.NewArticleHTTPHandler(u)
+	// Assertions
+	if assert.NoError(t, h.HandleGetArticleByID(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "{\"status\":200,\"message\":\"Success\",\"data\":{\"id\":0,\"title\":\"my-title\",\"content\":\"my-content\",\"author\":\"my-author\"}}", strings.Trim(rec.Body.String(), "\n"))
+	}
+}
+
+func TestArticleHandler_HandleGetArticleByID_ArticleDoesNotExist(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/articles/:article_id")
+	c.SetParamNames("article_id")
+	c.SetParamValues("1")
+
+	u := new(mocks.Usecase)
+	u.On("GetArticleByID", 1).Return(nil, nil)
+
+	h := handler.NewArticleHTTPHandler(u)
+	// Assertions
+	if assert.NoError(t, h.HandleGetArticleByID(c)) {
+		assert.Equal(t, http.StatusNotFound, rec.Code)
+		assert.Equal(t, "{\"status\":404,\"message\":\"article is not found\",\"data\":null}", strings.Trim(rec.Body.String(), "\n"))
+	}
+}
+
+func TestArticleHandler_HandleGetAllArticle(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/articles", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	result := []*model.Article{
+		&model.Article{Title: "my-title", Content: "my-content", Author: "my-author"},
+		&model.Article{Title: "my-title-2", Content: "my-content-2", Author: "my-author-2"},
+	}
+
+	u := new(mocks.Usecase)
+	u.On("GetAllArticle").Return(result, nil)
+
+	h := handler.NewArticleHTTPHandler(u)
+	// Assertions
+	if assert.NoError(t, h.HandleGetAllArticle(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "{\"status\":200,\"message\":\"Success\",\"data\":[{\"id\":0,\"title\":\"my-title\",\"content\":\"my-content\",\"author\":\"my-author\"},{\"id\":0,\"title\":\"my-title-2\",\"content\":\"my-content-2\",\"author\":\"my-author-2\"}]}", strings.Trim(rec.Body.String(), "\n"))
+	}
+}
+
+func TestArticleHandler_HandleGetAllArticle_RepositoryReturnError(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/articles", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	result := []*model.Article{}
+
+	u := new(mocks.Usecase)
+	u.On("GetAllArticle").Return(result, errors.New("whoops"))
+
+	h := handler.NewArticleHTTPHandler(u)
+	// Assertions
+	if assert.NoError(t, h.HandleGetAllArticle(c)) {
+		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		assert.Equal(t, "{\"status\":500,\"message\":\"whoops\",\"data\":null}", strings.Trim(rec.Body.String(), "\n"))
 	}
 }
